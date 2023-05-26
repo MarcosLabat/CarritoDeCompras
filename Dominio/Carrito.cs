70534cb73c7bc4e04db15e8cb4d8a1733a0e9616
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2023-05-26 18:40:06 -0300

    Clase Carrito y negocio

---
+++ Dominio/Carrito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Carrito
+    {
+        public List<Articulo> Articulos { get; set; }
+
+        public Carrito() 
+        {
+            Articulos = new List<Articulo>();
+        }
+    }
+}
---
+++ Negocio/CarritoNegocio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class CarritoNegocio
+    {
+        private Carrito Carrito;
+
+        public void AgregarArticulo(Articulo articulo)
+        {
+            Carrito.Articulos.Add(articulo);
+        }
+
+        public void QuitarArticulo(Articulo articulo)
+        {
+            Carrito.Articulos.Remove(articulo);
+        }
+
+        public List<Articulo> ObtenerArticulos()
+        {
+            return Carrito.Articulos;
+        }
+    }
+}
