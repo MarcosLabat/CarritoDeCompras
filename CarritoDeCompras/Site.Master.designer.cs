ed86c796d405dd68a528ab9cf7168977ae107065
Marcos <marcosluciano000@gmail.com>
2023-06-03 00:22:12 -0300

    eliminar articulo del carrito.

--- CarritoDeCompras/Site.Master.designer.cs
+++ CarritoDeCompras/Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace CarritoDeCompras
         protected global::System.Web.UI.WebControls.Panel pnlCarrito;
 
         /// <summary>
+        /// Control rptModal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptModal;
+
+        /// <summary>
         /// Control repMarcas.
         /// </summary>
         /// <remarks>
